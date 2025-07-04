eb6b84e36016c3f4e2e7c103c691f82f40a3755d
Damilola640 <Olatoyeclement531@gmail.com>
2025-07-04 09:48:09 +0100

    task 2 mysql

---
+++ task_2.sql
@@ -0,0 +1,46 @@
+-- USE THE DATABASE
+USE alx_book_store;
+
+-- CREATE THE TABLE: AUTHORS
+CREATE TABLE authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(50) NOT NULL
+);
+
+-- CREATE THE TABLE: BOOKS
+CREATE TABLE books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(215) NOT NULL,
+    author_id INT,
+    publication_date DATE,
+    genre VARCHAR(100),
+    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+);
+
+-- CREATE THE TABLE: CUSTOMERS
+CREATE TABLE customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    phone_number VARCHAR(15) UNIQUE NOT NULL,
+    address TEXT
+);
+
+-- CREATE THE TABLE: ORDERS
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    total_amount DECIMAL(10, 2) NOT NULL,
+    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+);
+
+-- CREATE THE TABLE: ORDER_DETAILS
+CREATE TABLE order_details (
+    order_detail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT NOT NULL,
+    book_id INT NOT NULL,
+    quantity INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id)
+);
