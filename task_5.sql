aeb8e21cc2a269cc1c4a527ce7e0912d998fbbfc
Damilola640 <Olatoyeclement531@gmail.com>
2025-07-04 16:29:36 +0100

    changes made

--- task_5.sql
+++ task_5.sql
@@ -1,3 +1,3 @@
 -- INSERT A SINGLE CUSTOMER INTO THE CUSTOMERS TABLE
 INSERT INTO customers (customer_id, customer_name, email, phone_number, address)
-VALUES (1, 'Ogundare Oluwafemi', 'ogundare@samanthachristmastechnologies.com', '+2349054511741', '23 Fagbeyiro St alakuko Lagos');
+VALUES (1, 'Cole Baidoo', 'cole.baidoo@sandtech.com', '234-567-8901', '123 Happiness Ave');
